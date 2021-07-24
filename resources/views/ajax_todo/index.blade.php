2249fdd43699a238296647ebef6d49128bcf0443
Puspita Vabna <sharif7761@gmail.com>
2021-07-24 13:46:43 +0600

    update modal data

--- resources/views/ajax_todo/index.blade.php
+++ resources/views/ajax_todo/index.blade.php
@@ -14,11 +14,11 @@
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">Cras justo odio</li>
-                            <li class="list-group-item">Dapibus ac facilisis in</li>
-                            <li class="list-group-item">Morbi leo risus</li>
-                            <li class="list-group-item">Porta ac consectetur ac</li>
-                            <li class="list-group-item">Vestibulum at eros</li>
+                            <li class="list-group-item todo-item" data-toggle="modal" data-target="#exampleModal">Cras justo odio</li>
+                            <li class="list-group-item todo-item" data-toggle="modal" data-target="#exampleModal">Dapibus ac facilisis in</li>
+                            <li class="list-group-item todo-item" data-toggle="modal" data-target="#exampleModal">Morbi leo risus</li>
+                            <li class="list-group-item todo-item" data-toggle="modal" data-target="#exampleModal">Porta ac consectetur ac</li>
+                            <li class="list-group-item todo-item" data-toggle="modal" data-target="#exampleModal">Vestibulum at eros</li>
                         </ul>
                     </div>
                 </div>
@@ -31,19 +31,37 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="title">Add New Item</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <input type="text" class="form-control" placeholder="Enter Item" id="item_input" name="item_input">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-warning" id="delete-btn">Delete</button>
+                    <button type="button" class="btn btn-info" id="update-btn">Save changes</button>
+                    <button type="button" class="btn btn-primary" id="add-btn">Add Item</button>
                 </div>
             </div>
         </div>
     </div>
+
+@endsection
+@section('custom_js')
+    <script>
+        $(document).ready(function(){
+            $('.todo-item').each(function (){
+                $(this).click(function (event){
+                    let text = $(this).text();
+                    $('#item_input').val(text)
+                    $('#title').text('Edit Item')
+                    $('#delete-btn').show()
+                    $('#update-btn').show()
+                    $('#add-btn').hide()
+                })
+            })
+        })
+    </script>
 @endsection
